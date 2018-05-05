3c3858075fabb799d258db495559772575103875
Spyros Argalias <sargalias@gmail.com>
2018-05-05 03:03:41 +0100

    setup basic project

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log(global);
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+
+if (require.main === module) {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server started on port ${process.env.PORT}`);
+    });
+} else {
+    module.exports = app;
+}
---
+++ app.test.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+test('app is a function', () => {
+    expect(app).toBeTruthy();
+});
