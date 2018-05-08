ba30ffb3580a353d2deac7b62468b01264a92595
Spyros Argalias <sargalias@gmail.com>
2018-05-08 03:42:34 +0100

    set up more project structure

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ require('dotenv').config();
 const app = express();
 
 
+app.set('view engine', 'pug');
+
+// Routes
+const routes = require('./routes/index');
+app.use(routes);
+
+
+
 if (require.main === module) {
     app.listen(process.env.PORT, () => {
         console.log(`Server started on port ${process.env.PORT}`);
--- app.test.js
+++ app.test.js
@@ -1,5 +1,28 @@
+const request = require('supertest');
 const app = require('./app');
+
 
 test('app is a function', () => {
     expect(app).toBeTruthy();
+});
+
+describe('Route: /', () => {
+    test('Should respond with statusCode 200 GET method', (done) => {
+        request(app).get('/').then((res) => {
+            expect(res.statusCode).toBe(200);
+            done();
+        })
+    });
+    test('Should not respond to DELETE method', (done) => {
+        request(app).delete('/').then((res) => {
+            expect(res.statusCode).toBe(404);
+            done();
+        });
+    });
+    test('Should not match the word error', (done) => {
+        request(app).get('/').then((res) => {
+            expect(res.text.toLowerCase()).not.toMatch('error');
+            done();
+        });
+    });
 });
