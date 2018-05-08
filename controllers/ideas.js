294c4dc31ed4e92d60c30927d8032c00c3f0f39a
Spyros Argalias <sargalias@gmail.com>
2018-05-08 11:39:28 +0100

    refactor routes and controllers

---
+++ controllers/ideas.js
@@ -0,0 +1,99 @@
+const { body, validationResult } = require('express-validator/check');
+const { matchedData, sanitizeBody } = require('express-validator/filter');
+const Idea = require('../models/Idea');
+
+
+module.exports.formValidation = [
+    body('title', 'Title is required').trim().isLength({min: 1}),
+    body('details', 'Details are required').trim().isLength({min: 1}),
+    sanitizeBody('title').trim().escape(),
+    sanitizeBody('details').trim().escape()
+];
+
+module.exports.index = (req, res) => {
+    Idea
+        .find()
+        .sort({date: -1})
+        .exec((err, ideas) => {
+            if (err) {
+                return next(err);
+            }
+            res.render('ideas/index', {ideas: ideas});
+        });
+};
+
+module.exports.new = (req, res) => {
+    res.render('ideas/new');
+};
+
+module.exports.create = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.render('ideas/new', {
+            errors: errors.array(),
+            title: req.body.title,
+            details: req.body.details
+        });
+    }
+    const idea = new Idea(req.body);
+    idea.save((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success_msg', 'Video idea added');
+        res.redirect('/ideas');
+    });
+};
+
+module.exports.edit = (req, res, next) => {
+    Idea.findById(req.params.idea_id, (err, idea) => {
+        if (err) {
+            return next(err);
+        }
+        if (idea === null) {
+            let err = new Error('Idea could not be found');
+            err.statusCode = 404;
+            return next(err);
+        }
+        res.render('ideas/edit', {idea: idea});
+    });
+};
+
+module.exports.update = (req, res, next) => {
+    Idea.findById(req.params.idea_id, (err, idea) => {
+        if (err) {
+            return next(err);
+        }
+        if (idea === null) {
+            let err = new Error('Idea could not be found');
+            err.statusCode = 404;
+            return next(err);
+        }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render('ideas/edit', {
+                errors: errors.array(),
+                idea: idea
+            });
+        }
+        idea.title = req.body.title;
+        idea.body = req.body.details;
+        idea.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash('success_msg', 'Video idea updated');
+            res.redirect('/ideas/');
+        });
+    });
+};
+
+module.exports.delete = (req, res, next) => {
+    Idea.findByIdAndRemove(req.params.idea_id, (err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success_msg', 'Video idea removed');
+        res.redirect('/ideas');
+    });
+};
--- routes/ideas.js
+++ routes/ideas.js
@@ -1,102 +1,17 @@
 const router = require('express').Router();
-const { body, validationResult } = require('express-validator/check');
-const { matchedData, sanitizeBody } = require('express-validator/filter');
-const Idea = require('../models/Idea');
-
-const formValidation = [
-    body('title', 'Title is required').trim().isLength({min: 1}),
-    body('details', 'Details are required').trim().isLength({min: 1}),
-    sanitizeBody('title').trim().escape(),
-    sanitizeBody('details').trim().escape()
-];
+const ic = ideasController = require('../controllers/ideas');
 
 
-router.get('/', (req, res) => {
-    Idea
-        .find()
-        .sort({date: -1})
-        .exec((err, ideas) => {
-            if (err) {
-                return next(err);
-            }
-            res.render('ideas/index', {ideas: ideas});
-        });
-});
+router.get('/', ic.index);
 
-router.get('/new', (req, res, next) => {
-    res.render('ideas/new');
-});
+router.get('/new', ic.new);
 
-router.post('/', formValidation, (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.render('ideas/new', {
-            errors: errors.array(),
-            title: req.body.title,
-            details: req.body.details
-        });
-    }
-    const idea = new Idea(req.body);
-    idea.save((err, idea) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success_msg', 'Video idea added');
-        res.redirect('/ideas');
-    });
-});
+router.post('/', ic.formValidation, ic.create);
 
-router.get('/:idea_id/edit', (req, res, next) => {
-    Idea.findById(req.params.idea_id, (err, idea) => {
-        if (err) {
-            return next(err);
-        }
-        if (idea === null) {
-            let err = new Error('Idea could not be found');
-            err.statusCode = 404;
-            return next(err);
-        }
-        res.render('ideas/edit', {idea: idea});
-    });
-});
+router.get('/:idea_id/edit', ic.edit);
 
-router.put('/:idea_id', formValidation, (req, res, next) => {
-    Idea.findById(req.params.idea_id, (err, idea) => {
-        if (err) {
-            return next(err);
-        }
-        if (idea === null) {
-            let err = new Error('Idea could not be found');
-            err.statusCode = 404;
-            return next(err);
-        }
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.render('ideas/edit', {
-                errors: errors.array(),
-                idea: idea
-            });
-        }
-        idea.title = req.body.title;
-        idea.body = req.body.details;
-        idea.save((err) => {
-            if (err) {
-                return next(err);
-            }
-            req.flash('success_msg', 'Video idea updated');
-            res.redirect('/ideas/');
-        });
-    });
-});
+router.put('/:idea_id', ic.formValidation, ic.update);
 
-router.delete('/:idea_id', (req, res, next) => {
-    Idea.findByIdAndRemove(req.params.idea_id, (err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success_msg', 'Video idea removed');
-        res.redirect('/ideas');
-    });
-});
+router.delete('/:idea_id', ic.delete);
 
 module.exports = router;
